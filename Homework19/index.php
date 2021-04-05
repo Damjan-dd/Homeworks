c3efc2d8bf4a014ef80ab747b7c8ce4a9281bb51
Damjan-dd <79701607+Damjan-dd@users.noreply.github.com>
2021-04-05 21:13:58 +0200

    Homework19

---
+++ Homework19/index.php
@@ -0,0 +1,35 @@
+<?php
+$one=10;
+$two=20;
+$three=30;
+$four=40;
+$five=5;
+$six=6;
+
+echo $two-$one;
+echo $four-$two;
+echo $three-$one;
+
+$result = $two-$one;
+echo $result;
+
+$result = $one*$one;
+echo $result;
+
+$result = $four/$two;
+echo $result;
+
+$result = $four+$two;
+echo $result;
+
+$result = $four+$one;
+echo $result;
+
+$result = $four*$four;
+echo $result;
+
+$result = $six*$five;
+echo $result;
+
+$result = $six*$one;
+echo $result;
